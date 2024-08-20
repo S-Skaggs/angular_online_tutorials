ba3c881c883998da6caf506889419497141de56d
S-Skaggs <s-skaggs@outlook.com>
2024-08-20 09:33:41 -0400

    FEAT: more progress son this tutorial, child routes and titles

--- routing-app/src/app/app.routes.ts
+++ routing-app/src/app/app.routes.ts
@@ -1,8 +1,32 @@
-import { Routes } from '@angular/router';
+import { ResolveFn, Routes } from '@angular/router';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ChildAComponent } from './child-a/child-a.component';
+import { ChildBComponent } from './child-b/child-b.component';
+
+// Create title for child-a
+const resolvedChildATitle: ResolveFn<string> = () => Promise.resolve('child a');
 
 export const routes: Routes = [
-  { path: 'first-component', component: FirstComponent },
-  { path: 'second-component', component: SecondComponent },
+  {
+    path: 'first-component',
+    title: 'First component',
+    component: FirstComponent, // this component has another <router-outlet /> in its template
+    children: [
+      {
+        path: 'child-a', // child route path
+        title: resolvedChildATitle,
+        component: ChildAComponent // child route component that the route renders
+      },
+      {
+        path: 'child-b', // child route path
+        title: 'child b',
+        component: ChildBComponent // child route component that the route renders
+      }
+    ]
+  }, // path to FirstComponent
+  { path: 'second-component', title: 'Second component', component: SecondComponent }, // path to SecondComponent
+  { path: '',   redirectTo: '/first-component', pathMatch: 'full' }, // redirect to FirstComponent
+  { path: '**', title: 'Page not found', component: PageNotFoundComponent } // wildcard route to PageNotFoundComponent
 ];
--- routing-app/src/app/first/first.component.ts
+++ routing-app/src/app/first/first.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-first',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './first.component.html',
   styleUrl: './first.component.css'
 })
